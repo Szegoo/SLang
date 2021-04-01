451883239b91612c96ecb259701bc41cf10bab10
Szegoo <73715684+Szegoo@users.noreply.github.com>
2021-04-01 19:25:26 +0200

    DECLARE

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace SLang
         static void Main(string[] args)
         {
             Console.WriteLine("Dobrodosao u SLang!");
-            System.Console.WriteLine("komande: print, :run");
+            System.Console.WriteLine("komande: PRINT, SUM, DECLARE, :run");
             string komanda = "";
-            string[] komande = new string[5];
+            string[] komande = new string[20];
             int index = 0;
             do
             {
@@ -26,20 +26,39 @@ namespace SLang
                 string kom = komande[i].Split(" ")[0];
                 switch (kom)
                 {
-                    case "print":
+                    case "PRINT":
                         int ind = komande[i].IndexOf(" ");
                         int end = komande[i].IndexOf(";");
                         string text = komande[i].Substring(ind + 1, (end - ind) - 1);
                         System.Console.WriteLine(text);
                         break;
-                    case "saberi":
-                        ind = komande[i].IndexOf(" ");
-                        end = komande[i].IndexOf(";");
-                        int num1 = Convert.ToInt32(komande[i].Substring(ind + 1, 1));
-                        string br2 = komande[i].Substring(ind + 3, 1);
-                        int num2 = Convert.ToInt32(br2);
+                    case "SUM":
+                        ind = komande[i].IndexOf(";");
+                        string[] koms = komande[i].Substring(0, ind).Split(" ");
+                        int num1 = Convert.ToInt32(koms[1]);
+                        int num2 = Convert.ToInt32(koms[2]);
                         int zbir = num1 + num2;
                         System.Console.WriteLine("zbir: " + zbir);
+                        break;
+                    case "DECLARE":
+                        ind = komande[i].IndexOf(";");
+                        koms = komande[i].Substring(0, ind).Split(" ");
+                        string ime = koms[1];
+                        string vrednost = koms[2];
+                        for (int m = 3; m < koms.Length; m++)
+                        {
+                            vrednost += " " + koms[m];
+                        }
+                        for (int j = i; j < index; j++)
+                        {
+                            if (komande[j].Contains(ime))
+                            {
+                                komande[j] = komande[j].Replace(ime, vrednost);
+                            }
+                        }
+                        break;
+                    default:
+                        System.Console.WriteLine(komande[i].Substring(0, komande[i].IndexOf(";")) + " - nije definisan!");
                         break;
                 }
             }
